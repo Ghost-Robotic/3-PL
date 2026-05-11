3c3d7aaadc287d9240ed4ef5ba0c9dcf3822fdf9
ghost-robotic <harrisonhsieh8@gmail.com>
2026-05-11 18:02:11 +1000

    add entry to set the name of print job

--- src/pages/add.py
+++ src/pages/add.py
@@ -5,12 +5,13 @@ from PIL import Image, ImageTk
 
 class Add(ctk.CTkFrame):
     def __init__(self, parent, controller):
+        self.gcode_source = None
+        self.file_name = ctk.StringVar()
+        self.print_name = ctk.StringVar()
         ctk.CTkFrame.__init__(self, parent, fg_color=style.dark_foreground)
         
-        self.gcode_source = None
-        
         container = ctk.CTkFrame(self, fg_color=style.dark_foreground)
-        container.grid(row=0, column=0, sticky='nsew')
+        container.grid(row=0, column=0, sticky='nsew', padx=25, pady=25)
         container.grid_columnconfigure(0, weight=5)
         container.grid_columnconfigure(1, weight=2)
         container.grid_rowconfigure(0, weight=1)
@@ -18,6 +19,13 @@ class Add(ctk.CTkFrame):
         # left side of form
         l_input_frame = ctk.CTkFrame(container, fg_color=style.dark_foreground)
         l_input_frame.grid(row=0, column=0, sticky='nsew')
+        l_input_frame.columnconfigure(1, weight=1)
+        
+        name_label = ctk.CTkLabel(l_input_frame, text="Print Name:", font=(style.normal_font, 25, "bold"), text_color="white")
+        name_label.grid(row=0, column=0, padx=(0,20))
+        self.print_title = ctk.CTkEntry(l_input_frame, textvariable=self.print_name,
+                                   font=(style.normal_font, 25), text_color="#f5f5f5", border_color=style.grey)
+        self.print_title.grid(row=0, column=1, sticky="ew")
         
         # right side of form
         r_input_frame = ctk.CTkFrame(container, fg_color=style.dark_foreground)
@@ -51,17 +59,18 @@ class Add(ctk.CTkFrame):
     def upload_file(self):
         self.gcode_source = ctk.filedialog.askopenfilename(title="Select G-code file",filetypes=[("g-Code files","*.gcode")])
 
-        name = ""
         if self.gcode_source is not "":
             grey_plus = ImageTk.PhotoImage((Image.open("assets\plusGrey.png")).resize((100,100), Image.LANCZOS))
             self.add_file_button.configure(fg_color=style.dark_foreground, border_color="#a7a7a7", image=grey_plus)
             
+            name = ""
             counter = len(self.gcode_source)-1
             while self.gcode_source[counter] != "/" or counter == -1:
                 name = self.gcode_source[counter] + name
                 counter -= 1
-                print(name)
             self.file_name.set(name)
+            if self.print_title.get() == "":
+                self.print_name.set(name[0:-6])
             
         
     def submit_form(self):
